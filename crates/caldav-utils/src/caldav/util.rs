6514a24f85d5bac2d0c34e79beef09366d1a1c89
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-01-20 16:02:45 -0600

    test: simple event creation test

--- crates/caldav-utils/src/caldav/util.rs
+++ crates/caldav-utils/src/caldav/util.rs
@@ -22,3 +22,27 @@ pub fn create_event(
 
     calendar
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_event() {
+        // We don't want to test the entire icalendar crate, so we'll just
+        // test that the events we create can be parsed back into a calendar
+        let id = "123";
+        let name = "Test Event";
+        let description = "This is a test event\nfoo";
+        let start = chrono::Utc::now();
+        let end = start + chrono::Duration::hours(1);
+
+        let calendar = create_event(id, name, description, start, end);
+        println!("{}", calendar.to_string());
+
+        let cstr = calendar.to_string();
+        let parsed = icalendar::parser::read_calendar(&cstr).unwrap();
+
+        assert!(true);
+    }
+}
